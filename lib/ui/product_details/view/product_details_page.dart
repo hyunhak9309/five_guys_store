44c9be321facda36d71f970011c72baaed978d76
Hyunhak <98297516+hyunhak9309@users.noreply.github.com>
2025-04-03 17:13:47 +0900

    상품 상세 페이지 로직 구현

--- lib/ui/product_details/view/product_details_page.dart
+++ lib/ui/product_details/view/product_details_page.dart
@@ -1,13 +1,15 @@
 import 'package:fiveguysstore/data/entity/product_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ProductDetailsPage extends StatelessWidget {
+class ProductDetailsPage extends ConsumerWidget {
   const ProductDetailsPage({super.key, required this.product});
   static const path = '/product_details';
   final ProductEntity product;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Product Details'),
