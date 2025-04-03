9f925b7bc9e937ba80b4f9f5900c0c8a3cc46fd8
qbg5652 <qbg5652@naver.com>
2025-04-03 18:07:43 +0900

    상품목록 리스트 수정.

--- lib/ui/home/view/home_page.dart
+++ lib/ui/home/view/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:fiveguysstore/ui/core/view/c_inkwell.dart';
 import 'package:fiveguysstore/ui/home/view/widget/cart_icon.dart';
 import 'package:fiveguysstore/ui/home/view_model/home_view_model.dart';
@@ -5,6 +7,7 @@ import 'package:fiveguysstore/ui/product_registration/view/product_registration_
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:loading_indicator/loading_indicator.dart';
 
 class HomePage extends ConsumerWidget {
@@ -25,12 +28,52 @@ class HomePage extends ConsumerWidget {
         data: (products) {
           // TODO: products를 인자로 받는 상품 카드 위젯을 구현,
           // TODO: 상품 카트 위젯 클릭 시, 상품 상세 페이지로 이동(인자 라우팅 이동간에 전달 필요, 필요하면 질문주세요!)
-          // TODO: products 갯수가 0일 경우 리스트 뷰 빌더가 아니라 center에 텍스트 위젯으로 상품 없는 상태 구현 필요
-          return ListView.builder(
+
+          if (products.isEmpty) {
+            return const Center(child: Text('상품이 없습니다.'));
+          } // 상품이 없을 경우, 화면 구현.
+
+          return ListView.separated(
+            separatorBuilder: (context, index) => const Divider(height: 1),
             itemCount: products.length,
             itemBuilder: (context, index) {
               final product = products[index];
-              return Container();
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        // color: Colors.grey.shade300,
+                        image: DecorationImage(
+                          image: MemoryImage(
+                            base64Decode(product.image.split(',').last),
+                            // 이미지 파일이 base64(문자열)로 되어있어, 이를 디코딩함. convert 임포팅.
+                            // data:image/jpeg;base64,를 잘라내기 위해 split 사용
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Text(
+                        product.name,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    Text(
+                      product.price == 0
+                          ? '무료배송' // 가격이 0원이라면 무료배송.
+                          : '${NumberFormat('#,###').format(product.price)} 원', //천단위마다 , 쓰기 위해 intl 패키지 설치 및 수정
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              );
             },
           );
         },
