2364f43ef3fbba2810335ff2a132f940f556588a
essaisko <essaisko@gmail.com>
2025-04-04 19:50:30 +0900

    Update http package version and add Copy Pods Resources build phase

--- lib/ui/shopping_cart/view/shopping_cart_page.dart
+++ lib/ui/shopping_cart/view/shopping_cart_page.dart
@@ -16,6 +16,7 @@ class ShoppingCartPage extends ConsumerWidget {
         itemCount: viewModel.length,
         itemBuilder: (context, index) {
           final product = viewModel[index];
+          return null;
           // TODO: products를 인자로 받는 상품 카드 위젯을 구현,
           // TODO: 상품 카트 위젯에서 수량 변경 버튼 클릭 시, 수량 변경 로직 호출 viewModelFC.updateProductNum(productId: product.id, plus: 증가면 true, 감소면 false)
           // TODO: 상품 카트 위젯에서 삭제 버튼 클릭 시, 삭제 로직 호출 viewModelFC.deleteProduct(productId: product.id)
