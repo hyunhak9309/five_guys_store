f621dd7065747e92d9988ade7ffc34d2a777e80b
Hyunhak <98297516+hyunhak9309@users.noreply.github.com>
2025-04-04 19:25:57 +0900

    Revert "Merge pull request #10 from hyunhak9309/feature/home"
    This reverts commit c9b2a8ed78010d3abaf1c7e9fc65bda0eee3b708, reversing
    changes made to 99e7287a736dd0dbe39e44947231d00bda875bd9.

--- lib/domain/repository/product_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fiveguysstore/data/entity/product_entity.dart';
-
-abstract class ProductRepository {
-  Future<List<ProductEntity>> getProducts();
-
-  Future<void> updateProduct(List<ProductEntity> products);
-}
