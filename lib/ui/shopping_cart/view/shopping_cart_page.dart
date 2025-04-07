e93e10a92ac5ca1d0f9e21ccbaca737612f66455
Hyunhak <98297516+hyunhak9309@users.noreply.github.com>
2025-04-07 11:23:39 +0900

    장바구니 구현

--- lib/ui/shopping_cart/view/shopping_cart_page.dart
+++ lib/ui/shopping_cart/view/shopping_cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:fiveguysstore/ui/shopping_cart/view/widget/item_card.dart';
 import 'package:fiveguysstore/ui/shopping_cart/view_model/shopping_cart_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -9,9 +10,9 @@ class ShoppingCartPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final viewModel = ref.watch(shoppingCartViewModelProvider);
-    final viewModelFC = ref.read(shoppingCartViewModelProvider.notifier);
     return Scaffold(
       appBar: AppBar(title: const Text('Shopping Cart')),
+<<<<<<< Updated upstream
       body: ListView.builder(
         itemCount: viewModel.length,
         itemBuilder: (context, index) {
@@ -22,6 +23,20 @@ class ShoppingCartPage extends ConsumerWidget {
           // TODO: 상품 카트 위젯에서 삭제 버튼 클릭 시, 삭제 로직 호출 viewModelFC.deleteProduct(productId: product.id)
         },
       ),
+=======
+      body:
+          viewModel.isEmpty
+              ? const Center(child: Text('장바구니가 비었습니다.'))
+              : ListView.builder(
+                itemCount: viewModel.length,
+                itemBuilder: (context, index) {
+                  final product = viewModel[index];
+                  return ItemCard(item: product);
+                },
+              ),
+      // TODO: 상품 카트 위젯에서 수량 변경 버튼 클릭 시, 수량 변경 로직 호출 viewModelFC.updateProductNum(productId: product.id, plus: 증가면 true, 감소면 false)
+      // TODO: 상품 카트 위젯에서 삭제 버튼 클릭 시, 삭제 로직 호출 viewModelFC.deleteProduct(productId: product.id)
+>>>>>>> Stashed changes
     );
   }
 }
