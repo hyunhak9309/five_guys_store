5ceb7de1a86628eeb6791583ac1ae447af2a4127
Hyunhak <98297516+hyunhak9309@users.noreply.github.com>
2025-04-07 13:30:59 +0900

    상품 상세 페이지 구현

--- lib/ui/shopping_cart/view/shopping_cart_page.dart
+++ lib/ui/shopping_cart/view/shopping_cart_page.dart
@@ -12,18 +12,6 @@ class ShoppingCartPage extends ConsumerWidget {
     final viewModel = ref.watch(shoppingCartViewModelProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Shopping Cart')),
-<<<<<<< Updated upstream
-      body: ListView.builder(
-        itemCount: viewModel.length,
-        itemBuilder: (context, index) {
-          final product = viewModel[index];
-          return null;
-          // TODO: products를 인자로 받는 상품 카드 위젯을 구현,
-          // TODO: 상품 카트 위젯에서 수량 변경 버튼 클릭 시, 수량 변경 로직 호출 viewModelFC.updateProductNum(productId: product.id, plus: 증가면 true, 감소면 false)
-          // TODO: 상품 카트 위젯에서 삭제 버튼 클릭 시, 삭제 로직 호출 viewModelFC.deleteProduct(productId: product.id)
-        },
-      ),
-=======
       body:
           viewModel.isEmpty
               ? const Center(child: Text('장바구니가 비었습니다.'))
@@ -34,9 +22,6 @@ class ShoppingCartPage extends ConsumerWidget {
                   return ItemCard(item: product);
                 },
               ),
-      // TODO: 상품 카트 위젯에서 수량 변경 버튼 클릭 시, 수량 변경 로직 호출 viewModelFC.updateProductNum(productId: product.id, plus: 증가면 true, 감소면 false)
-      // TODO: 상품 카트 위젯에서 삭제 버튼 클릭 시, 삭제 로직 호출 viewModelFC.deleteProduct(productId: product.id)
->>>>>>> Stashed changes
     );
   }
 }
